5a0b6a016613cb1a6fedc4355db498e449db0795
Wesley Cho <57739900+wes-cho@users.noreply.github.com>
2025-04-05 17:22:02 -0400

    Put together relevant weather data into an object

--- src/weather.js
+++ src/weather.js
@@ -1,5 +1,4 @@
 export const greeting = "weather.js loaded";
-
 class weatherObject {
     constructor(temp, feelsLike, high, low, description){
         this.temp = temp;
@@ -14,10 +13,19 @@ async function getWeather(location){
     try {
         const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/?key=SX3RZTE2R4U2XD2K5QUF9SP4G`, {mode: 'cors'});
         const weatherData = await response.json();
-        console.log(weatherData);
+        const today = weatherData.days[0];
+        const weather = new weatherObject(
+            today.temp,
+            today.feelslike,
+            today.tempmax,
+            today.tempmin,
+            today.description
+        );
+        
+        return weather;
+
     } catch (error) {
         console.log(error)
     };
 };
 getWeather('astoria');
-
