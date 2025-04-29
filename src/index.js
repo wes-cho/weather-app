3c39ee187062bbf5866b7f05cdebd6e86c6b9fea
Wesley Cho <57739900+wes-cho@users.noreply.github.com>
2025-04-29 10:53:46 -0400

    Added icons for various weather conditions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import "./styles.css";
 import { link, getWeather } from "./weather.js";
-import sunIcon from './weather-icons/Weather Icon Sun.png';
+import sunPng from './weather-icons/Clear.png';
+import cloudPng from './weather-icons/Overcast.png';
+import partlyPng from './weather-icons/Partly Cloudy.png';
+import rainPng from './weather-icons/Rain.png';
+import snowPng from './weather-icons/Snow.png';
+import windPng from './weather-icons/Wind.png';
 
 console.log(link);
 
@@ -29,13 +34,43 @@ go.addEventListener("click", () => {
         hiLow.setAttribute('id', 'hiLow');
         currentTempsContainer.appendChild(hiLow);
 
-        if (weather.conditions === 'Clear'){
+        if (weather.conditions.includes('Rain')){
+            const rainIcon = document.createElement('img');
+            rainIcon.src = rainPng;
+            rainIcon.setAttribute('alt', 'rain');
+            rainIcon.setAttribute('class', 'conditions-icon');
+            container.appendChild(rainIcon);
+        } else if (weather.conditions === 'Clear'){
             const clearIcon = document.createElement('img');
-            clearIcon.src = sunIcon;
+            clearIcon.src = sunPng;
             clearIcon.setAttribute('alt', 'sun');
             clearIcon.setAttribute('class', 'conditions-icon')
             container.appendChild(clearIcon); 
-        }
+        } else if (weather.conditions.includes('Overcast')){
+            const overcastIcon = document.createElement('img');
+            overcastIcon.src = cloudPng;
+            overcastIcon.setAttribute('alt', 'clouds');
+            overcastIcon.setAttribute('class', 'conditions-icon');
+            container.appendChild(overcastIcon);
+        } else if (weather.conditions.includes('Partially Cloudy')){
+            const partlyCloudyIcon = document.createElement('img');
+            partlyCloudyIcon.src = partlyPng;
+            partlyCloudyIcon.setAttribute('alt', 'sun behind clouds');
+            partlyCloudyIcon.setAttribute('class', 'conditions-icon');
+            container.appendChild(partlyCloudyIcon);
+        } else if (weather.conditions.includes('Snow')){
+            const snowIcon = document.createElement('img');
+            snowIcon.src = snowPng;
+            snowIcon.setAttribute('alt', 'snow');
+            snowIcon.setAttribute('class', 'conditions-icon');
+            container.appendChild(snowIcon);
+        } else if (weather.conditions.includes('Wind')){
+            const windIcon = document.createElement('img');
+            windIcon.src = windPng;
+            windIcon.setAttribute('alt', 'wind');
+            windIcon.setAttribute('class', 'conditions-icon');
+            container.appendChild(windIcon);
+        };
     });
 });
 
